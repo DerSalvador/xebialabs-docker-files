30f0acae6270fbfbd5b4257adba5663eac628c59
Michael Santana <michael.santana@dersalvador.com>
2019-10-01 22:58:23 +0100

    using xip.io ingress for xl-deploy web

--- xldeploy/buildAndPush.sh
+++ xldeploy/buildAndPush.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-REPOTAG=xl-deploy:debug
+REPOTAG=xl-deploy:9.0.5-centos-debug
 docker build . -t docker.io/brokerme/$REPOTAG
 loginToDockerRegistryBrokerme.sh 
 docker push  docker.io/brokerme/$REPOTAG

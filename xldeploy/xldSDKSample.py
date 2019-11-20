9887739156f143e38ad00cd709e78b147b98a8f7
Michael Santana <michael.santana@dersalvador.com>
2019-11-20 18:40:10 +0100

    xld logging with deprecated tasks object

--- xldeploy/xldSDKSample.py
+++ xldeploy/xldSDKSample.py
@@ -13,9 +13,28 @@ print(ci.id)
 
 # deployment
 deployment = client.deployment
+print(dir(deployment))
+try:
+   undepl = deployment.prepare_undeploy("Environments/PRD/windows-webform")
+   untask = deployment.create_task(undepl)
+   untask.start()
+except Exception as ex :
+   print(ex)
 deploymentRef = deployment.prepare_initial("Applications/windows-webform/1.1", "Environments/PRD")
 depl = deployment.prepare_auto_deployeds(deploymentRef)
 task = deployment.create_task(depl)
 task.start()
-task.export(null,null,null)
-print(task.task_id)
+print(dir(task))
+print(dir(task.task_service))
+print(dir(task.get_steps("0_1_1")))
+print(dir(deployment))
+# deployment.waitForTask(task.id)
+s = task.get_steps("0_1_1")
+print s
+print s["steps"]
+print s["steps"][0]
+print s["steps"][0]["state"]
+print(dir(task))
+print(dir(task.task_service))
+q = task.task_service.query()
+print type(q)
